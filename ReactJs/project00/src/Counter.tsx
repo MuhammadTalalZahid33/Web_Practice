3f714d8bc3be29d0f746f6da2c7bbc85a76f2519
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-08-14 14:42:58 +0500

    updated changes...

--- ReactJs/project00/src/Counter.tsx
+++ ReactJs/project00/src/Counter.tsx
@@ -4,7 +4,7 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-yellow-100 via-yellow-300 to-yellow-500">
+        <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-red-100 via-red-300 to-red-500">
             <div className="bg-white p-8 rounded-xl shadow-lg text-center max-w-sm w-full">
                 <h2 className="text-2xl font-bold text-gray-800 mb-4 animate-pulse">Let's count the clicks 🎯</h2>
                 <p className="text-lg text-gray-700 mb-6 transition-transform duration-300 ease-in-out hover:scale-105">
@@ -13,12 +13,34 @@ function Counter() {
                         {count}
                     </span>
                 </p>
+                <div className="flex justify-center gap-4">
                 <button
-                    className="bg-yellow-600 hover:bg-yellow-700 transition-all duration-300 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:scale-105 active:scale-95"
+                    className="bg-yellow-600 hover:bg-yellow-700 transition-all duration-300 text-white font-semibold py-1 px-4 rounded-md shadow-md hover:scale-105 active:scale-95"
                     onClick={() => setCount(count + 1)}
                 >
-                    Click me!
+                    {"Increment"}
                 </button>
+                <button
+                    className="bg-green-600 hover:bg-green-700 transition-all duration-300 text-white font-semibold py-1 px-4 rounded-md shadow-md hover:scale-105 active:scale-95"
+                    // disabled = {count === 0}
+                    // onClick = {()=> setCount(count - 1)}
+                    // OR
+                    // onClick={() => setCount(prev => Math.max(prev - 1, 0))}
+                    // OR
+                    // onClick = {() => setCount( prev => {
+                    //     if(prev <= 0) return 0; 
+                    //         return prev - 1;
+                    // })}
+                    // OR
+                    onClick={() => {
+                        if(count > 0){
+                        setCount(count - 1);
+                    }
+                    }}
+                >
+                    {"Decrement"}
+                </button>
+                </div>
             </div>
         </div>
     );
